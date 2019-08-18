befe4977605839837b027c6f3897cdaf7ee8bc7b
Gisa Klement <gisa.klement@signavio.com>
2019-08-18 19:27:50 +0200

    changes question ids

--- src/wizardQuestions.js
+++ src/wizardQuestions.js
@@ -1,16 +1,16 @@
 export default [
   {
-    id: '1',
+    id: '0',
     question: 'Question 1',
     answers: ['Answer 1-1', 'Answer 1-2'],
   },
   {
-    id: '2',
+    id: '1',
     question: 'Question 2',
     answers: ['Answer 2-1', 'Answer 2-2'],
   },
   {
-    id: '3',
+    id: '2',
     question: 'Question 3',
     answers: ['Answer 3-1', 'Answer 3-2'],
   },
