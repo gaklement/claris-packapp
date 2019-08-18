6bd09d3bae01dc45fd7194bcd9efbfa0e3cb258a
Gisa Klement <gisa.klement@signavio.com>
2019-08-18 20:33:40 +0200

    remembers answers

--- src/Wizard.js
+++ src/Wizard.js
@@ -4,16 +4,40 @@ import React from 'react'
 import { defaultStyle } from 'substyle'
 import wizardQuestions from './wizardQuestions'
 
-function Wizard({ currentQuestionId, setGivenAnswers, style }) {
+function Wizard({
+  currentQuestionId,
+  givenAnswers,
+  pendingAnswer,
+  setGivenAnswers,
+  setPendingAnswer,
+  setCurrentQuestionId,
+  style,
+}) {
   return (
     <div {...style}>
       {wizardQuestions[currentQuestionId].question}
       {wizardQuestions[currentQuestionId].answers.map((answer, key) => (
         <div key={key} {...style('answer')}>
-          <input type="radio" name="answer" value={`answer-${key}`} />
+          <input
+            type="radio"
+            name="answer"
+            value={key}
+            onClick={() => setPendingAnswer(key)}
+          />
           <div>{answer}</div>
         </div>
       ))}
+      <button
+        onClick={ev => {
+          setGivenAnswers({
+            question: currentQuestionId,
+            answered: pendingAnswer,
+          })
+        }}
+        type="text"
+      >
+        Next
+      </button>
     </div>
   )
 }
@@ -31,5 +55,6 @@ const styled = defaultStyle(() => ({
 export default compose(
   withState('currentQuestionId', 'setCurrentQuestionId', 0),
   withState('givenAnswers', 'setGivenAnswers', []),
+  withState('pendingAnswer', 'setPendingAnswer'),
   styled
 )(Wizard)
