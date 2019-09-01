8cad8cc00f91be840f207e185e52db712c6cfadb
Gisa Klement <gisa.klement@signavio.com>
2019-09-01 20:01:33 +0200

    adds initializeTestData

--- src/App.js
+++ src/App.js
@@ -119,3 +119,28 @@ export default compose(
   }),
   styled
 )(App)
+function initializeTestData() {
+  const packages = [
+    { id: 'basics', name: 'Basics', includePackageIds: ['cosmetic'] },
+    { id: 'cosmetic', name: 'Kosmetik' },
+  ]
+
+  const packagesRef = database.ref('packages')
+
+  packages.forEach(packageInstance => {
+    const packageRef = packagesRef.push()
+    packageRef.set(packageInstance)
+  })
+
+  const items = [
+    { id: 'shoes', name: 'Schuhe', packageIds: ['basics'] },
+    { id: 'floss', name: 'Zahnseide', packageIds: ['cosmetics'] },
+  ]
+
+  const itemsRef = database.ref('items')
+
+  items.forEach(item => {
+    const itemRef = itemsRef.push()
+    itemRef.set(item)
+  })
+}
