d9f8bc8604dacf6e6cd47a91ccd8c948d4315897
Gisa Klement <gisaklement@hotmail.com>
2019-09-12 23:45:53 +0200

    clear database before initializing data

--- src/dataUtils/initializeTestData.js
+++ src/dataUtils/initializeTestData.js
@@ -1,21 +1,24 @@
 import data from '../data'
 
 export default function initializeTestData(database) {
-  const packages = data.packages
+  const rootRef = database.ref()
+  rootRef.set({}, () => {
+    const packages = data.packages
 
-  const packagesRef = database.ref('packages')
+    const packagesRef = database.ref('packages')
 
-  packages.forEach(packageInstance => {
-    const packageRef = packagesRef.push()
-    packageRef.set(packageInstance)
-  })
+    packages.forEach(packageInstance => {
+      const packageRef = packagesRef.push()
+      packageRef.set(packageInstance)
+    })
 
-  const items = data.items
+    const items = data.items
 
-  const itemsRef = database.ref('items')
+    const itemsRef = database.ref('items')
 
-  items.forEach(item => {
-    const itemRef = itemsRef.push()
-    itemRef.set(item)
+    items.forEach(item => {
+      const itemRef = itemsRef.push()
+      itemRef.set(item)
+    })
   })
 }
