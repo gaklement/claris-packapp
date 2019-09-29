435ed136a549d4c0e63075c057301c195fbe5f5b
Gisa Klement <gisaklement@hotmail.com>
2019-09-29 16:29:23 +0200

    styles popUp button and shows adhoc on click

--- src/AdHoc.js
+++ src/AdHoc.js
@@ -1,25 +1,41 @@
-import { button, margin } from './theme'
+import { button, color, margin } from './theme'
 import { compose, withHandlers, withState } from 'recompose'
 
 import React from 'react'
 import { defaultStyle } from 'substyle'
 import { uniqueId } from 'lodash'
 
-function AdHoc({ itemName, onItemAdd, onInputChange, onKeyDown, style }) {
+function AdHoc({
+  adHocActive,
+  itemName,
+  onItemAdd,
+  onInputChange,
+  onKeyDown,
+  setAdHocActive,
+  style,
+}) {
   return (
-    <div {...style('adHoc')}>
-      <input
-        {...style('adHocInput')}
-        id="adHocName"
-        type="text"
-        onChange={onInputChange}
-        onKeyDown={onKeyDown}
-        value={itemName}
-      />
-
-      <div {...style('adHocAdd')} onClick={onItemAdd}>
+    <div>
+      <div {...style('popUp')} onClick={() => setAdHocActive(true)}>
         +
       </div>
+
+      {adHocActive && (
+        <div {...style('adHoc')}>
+          <input
+            {...style('adHocInput')}
+            id="adHocName"
+            type="text"
+            onChange={onInputChange}
+            onKeyDown={onKeyDown}
+            value={itemName}
+          />
+
+          <div {...style('adHocAdd')} onClick={onItemAdd}>
+            +
+          </div>
+        </div>
+      )}
     </div>
   )
 }
@@ -51,10 +67,27 @@ const styled = defaultStyle(() => {
       paddingBottom: 0,
       paddingLeft: button.padding,
     },
+    popUp: {
+      backgroundColor: color.primary,
+      border: `1px solid ${color.secondary}`,
+      borderRadius: '20px',
+      bottom: 0,
+      color: color.secondary,
+      fontSize: 25,
+      height: '40px',
+      lineHeight: '35px',
+      marginBottom: margin.medium,
+      marginRight: margin.medium,
+      position: 'fixed',
+      right: 0,
+      textAlign: 'center',
+      width: '40px',
+    },
   }
 })
 
 export default compose(
+  withState('adHocActive', 'setAdHocActive', false),
   withState('itemName', 'setItemName', 'Socken'),
   withHandlers({
     onInputChange: ({ setItemName }) => ({ target }) => {
